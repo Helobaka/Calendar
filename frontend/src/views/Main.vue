5f7bf116df5bc5b43208fca48a09271bf10c9051
helobaka <lahkandr010@mail.ru>
2020-08-06 22:11:32 +0300

    ref: убрал лишнее

--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -1,55 +1,72 @@
 <template>
-  <v-row>
-    <v-expansion-panels>
-      <v-expansion-panel>
-        <v-expansion-panel-header>Каледарь</v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <v-col>
-            <v-sheet height="64">
-              <v-toolbar flat color="white">
-                <v-btn text @click="prev">
-                  <v-icon>mdi-chevron-left</v-icon>
-                </v-btn>
-                <v-btn text @click="next">
-                  <v-icon>mdi-chevron-right</v-icon>
-                </v-btn>
-                <v-toolbar-title v-if="$refs.calendar">
-                  {{ $refs.calendar.title }}
-                </v-toolbar-title>
-              </v-toolbar>
-            </v-sheet>
-            <v-calendar
-              ref="calendar"
-              v-model="focus"
-              color="primary"
-              :type="type"
-            >
-              <template v-slot:day="{ date }">
-                <v-col>
-                  <v-text-field filled clearable v-model="tempObj[date]">
-                  </v-text-field>
-                </v-col>
-              </template>
-            </v-calendar>
-          </v-col>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
-    <v-expansion-panels>
-      <v-expansion-panel>
-        <v-expansion-panel-header>График</v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <v-col>
-            <v-btn outlined color="grey darken-2" @click="keysGet">нажми
+  <div>
+    <v-btn
+      block
+      v-on:click="showCalendar = !showCalendar"
+      color="#296443"
+      dark
+    >
+      Каледарь
+    </v-btn>
+    <v-slide-y-transition>
+      <div v-show="showCalendar">
+        <v-sheet height="64">
+          <v-toolbar flat dark color="#358357">
+            <v-btn text @click="prev">
+              <v-icon>mdi-chevron-left</v-icon>
             </v-btn>
-            <apexchart height="500" type="bar" :options="options" :series="series"></apexchart>
-          </v-col>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
-    <pre>{{ series }}}</pre>
+            <v-btn text @click="next">
+              <v-icon>mdi-chevron-right</v-icon>
+            </v-btn>
+            <v-toolbar-title v-if="$refs.calendar" >
+              {{ $refs.calendar.title }}
+            </v-toolbar-title>
+          </v-toolbar>
+        </v-sheet>
+        <v-calendar
+          dark
+          ref="calendar"
+          v-model="focus"
+          color="#358357"
+          :type="type"
+        >
+          <template v-slot:day="{ date }">
+            <v-col>
+              <v-text-field
+                :rules="floatRules"
+                filled
+                clearable
+                v-model="tempObj[date]"
+              >
+              </v-text-field>
+            </v-col>
+          </template>
+        </v-calendar>
+      </div>
+    </v-slide-y-transition>
+    <v-btn
+      block
+      v-on:click="showGraph = !showGraph"
+      color="#296443"
+      dark
+    >
+      График
+    </v-btn>
+    <v-slide-y-transition>
+      <apexchart
+        v-show="showGraph"
+        height="500"
+        type="bar"
+        :options="options"
+        :series="series"
+      ></apexchart>
+    </v-slide-y-transition>
+
+    <v-row>
+    <pre>{{ series }}</pre>
     <pre>{{ tempObj }}</pre>
-  </v-row>
+      </v-row>
+  </div>
 </template>
 
 <script>
@@ -58,9 +75,14 @@ export default {
   name: 'Main',
   data() {
     return {
+      showCalendar: false,
+      showGraph: false,
       focus: '',
       type: 'month',
       tempObj: {},
+      floatRules: [
+        (v) => /\d*\.\d*|\d*/.test(v) || 'Нужно число',
+      ],
       typeToLabel: {
         month: 'Month',
         week: 'Week',
@@ -69,34 +91,56 @@ export default {
       },
       options: {
         chart: {
+          background: '#b1cabc',
           id: 'vuechart-example',
         },
         xaxis: {
           type: 'datetime',
         },
       },
-      series: [{
-        name: 'Температура',
-        data: [],
-      }],
+      // series: [{
+      //   name: 'Температура',
+      //   data: [],
+      // }],
     };
   },
   mounted() {
-    this.$refs.calendar.checkChange();
+    // this.$refs.calendar.checkChange();
   },
+  computed: {
+    series() {
+      return [{
+        name: 'Температура',
+        color: '#296443',
+        data: [...Object.entries(this.tempObj)
+          .filter((arr) => arr[1] !== null && arr[1] !== '')
+          .sort()
+          .map(([x, y]) => ({ x, y }))],
+      }];
+    },
+  },
+  // watch: {
+  //   tempObj: {
+  //     immediate: false,
+  //     deep: true,
+  //     handler() {
+  //       this.series[0].data = [...Object.entries(this.tempObj)
+  //         .filter((arr) => arr[1] !== null)
+  //         .sort()
+  //         .map(([x, y]) => ({ x, y }))];
+  //       // this.$set(this.series[0], 'data', Object.entries(this.tempObj)
+  //       //   .filter((arr) => arr[1] !== null)
+  //       //   .sort()
+  //       //   .map(([x, y]) => ({ x, y })));
+  //     },
+  //   },
+  // },
   methods: {
     prev() {
       this.$refs.calendar.prev();
     },
     next() {
       this.$refs.calendar.next();
-    },
-    keysGet() {
-      this.series = [{
-        data: Object.entries(this.tempObj)
-          .filter((arr) => arr[1] !== null)
-          .map(([x, y]) => ({ x, y })),
-      }];
     },
   },
 };
--- frontend/src/views/Vacancy.vue
+++ frontend/src/views/Vacancy.vue
@@ -1,11 +1,53 @@
 <template>
-
-  <div>f</div>
+  <div class="text-center">
+    <v-flex>собачка</v-flex>
+    <v-btn
+          block
+          v-on:click="showCalendar = !showCalendar"
+          color="green"
+          dark
+        >
+          Каледарь
+        </v-btn>
+    <v-btn
+          block
+          v-on:click="showCalendar = !showCalendar"
+          color="green"
+          dark
+        >
+          Каледарь
+        </v-btn>
+    <v-slide-y-transition>
+      <v-calendar v-show="showCalendar">
+    </v-calendar>
+    </v-slide-y-transition>
+    <v-btn
+          block
+          v-on:click="showCalendar = !showCalendar"
+          color="green"
+          dark
+        >
+          Каледарь
+        </v-btn>
+    <v-btn
+          block
+          v-on:click="showCalendar = !showCalendar"
+          color="green"
+          dark
+        >
+          Каледарь
+        </v-btn>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Vacancy',
+  data() {
+    return {
+      showCalendar: false,
+    };
+  },
 };
 </script>
 
